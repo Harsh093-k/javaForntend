8fbbb0d7692303f96b74a55237b38e7fc9e31743
Harsh093-k <HarshKashyap701153@gmail.com>
2025-03-19 23:05:52 +0530

    done

--- src/components/screen/SignIn.js
+++ src/components/screen/SignIn.js
@@ -21,9 +21,9 @@ const SignInPage = () => {
       const response = await axios.post(
         "https://backendjava-omso.onrender.com/api/users/register",
         { name, email, password },
-        { headers: { "Content-Type": "application/json" } }
+        // { headers: { "Content-Type": "application/json" } }
       );
-  
+    console.log(name,email,password);
       if (response.data && response.data.id) {
         await axios.post(
           "https://backendjava-omso.onrender.com/app/api/orders/User-email",
